8db62748e9eead586bf5792a0572c7f0aa2758a7
WsMithril <WsMithril@gmail.com>
2014-02-25 21:08:32 +0800

    Check kernel version on runtime

--- reapsub.c
+++ reapsub.c
@@ -7,14 +7,39 @@
 
 #include <sys/prctl.h>
 
+#include <string.h>
+#include <sys/utsname.h>
+
+#include "reapsub.h"
+int check_kernel_above_3_4(void) {
+    int major = 0, minor = 0;
+    struct utsname uts;
+    uname(&uts);
+
+    // parse uts.release
+    const char * release = uts.release;
+    int p = 0;
+#define isnum(c) ((c) <= '9' && (c) >= '0')
+    while (release[p] && isnum(release[p]))
+        major = major * 10 + release[p++] - '0';
+    p++;
+    while (release[p] && isnum(release[p]))
+        minor = minor * 10 + release[p++] - '0';
+
+    if (major >= 3 && minor >= 4) {
+        return 0;
+    } else {
+        return 1;
+    }
+#undef isnum
+}
+
 #ifdef PR_SET_CHILD_SUBREAPER
 #include <sys/wait.h>
 #include <stdio.h>
 #include <errno.h>
-#include <string.h>
 #include <signal.h>
 
-#include "reapsub.h"
 
 int set_reap_subprocess(void) {
     int ret = 0;
